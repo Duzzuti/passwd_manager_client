5b8f21716c44ee09e494a3e45f05edc3f69723f6
Duzzuti <duzzuti@gmail.com>
2023-11-26 23:41:12 +0100

    Add dialog for adding an existing user

--- lib/login/dialogs/add_existing_user_dialog.dart
+++ lib/login/dialogs/add_existing_user_dialog.dart
@@ -1,9 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:passwd_manager_client/classes/user.dart';
+import 'package:passwd_manager_client/login/widgets/field_input_decorations/field_decos.dart';
+import 'package:passwd_manager_client/login/widgets/password_field.dart';
+import 'package:passwd_manager_client/login/widgets/username_field.dart';
+import 'package:passwd_manager_client/main_widgets/dialog_cancel.dart';
 
-class AddExistingUserDialog extends AlertDialog {
+class AddExistingUserDialog extends StatefulWidget {
+  static const double _padding = 8;
   const AddExistingUserDialog({
     super.key,
   });
+
+  @override
+  State<AddExistingUserDialog> createState() => _AddExistingUserDialogState();
+}
+
+class _AddExistingUserDialogState extends State<AddExistingUserDialog> {
+  
+  InputFieldState passwordFieldState = InputFieldState.ERROR;
+  String _password = "";
+  String _username = "";
+
+  void onChangePassword(String value){
+    setState(() {
+      _password = value;
+      if(value.isEmpty){
+        passwordFieldState = InputFieldState.ERROR;
+      }else{
+        passwordFieldState = InputFieldState.WARNING;
+      }
+    });
+  }
+
+  void onChangeUsername(String value){
+    setState(() {
+      _username = value;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,10 +46,37 @@ class AddExistingUserDialog extends AlertDialog {
       title: Text('Add Existing User', 
         style: Theme.of(context).textTheme.labelLarge,
       ),
-      content: Text('Do you wanna create a new user or add an existing one?',
-        style: Theme.of(context).textTheme.bodyMedium,
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('Please enter an username:',
+            style: Theme.of(context).textTheme.bodyMedium,
+          ),
+
+          const SizedBox(height: AddExistingUserDialog._padding),
+
+          ChooseUsernameField(
+            onTextChanged: onChangeUsername,
+            users: const [User(name: "UserA"), User(name: "UserB"), User(name: "UserC")],
+          ),
+
+          const SizedBox(height: AddExistingUserDialog._padding),
+
+          Text('Please enter a new password:',
+            style: Theme.of(context).textTheme.bodyMedium,
+          ),
+
+          const SizedBox(height: AddExistingUserDialog._padding),
+
+          PasswordField(
+            onTextChanged: onChangePassword, 
+            state: passwordFieldState
+          ),
+        ],
       ),
       actions: const <Widget>[
+        DialogCancel(),
       ],
     );
   }
